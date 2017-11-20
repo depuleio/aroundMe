be5ea2d47073f068d4ff714fabf62b187d5a5c44
Daniel Emanuel Puleio <depuleio@buffalo.edu>
2017-11-20 03:56:17 -0500

    official release

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,23 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        # Get the email
+        email = self.cleaned_data.get('email')
+
+        # Check to see if any users already exist with this email as a username.
+        try:
+            match = User.objects.get(email=email)
+        except User.DoesNotExist:
+            # Unable to find a user, this is fine
+            return email
+
+        # A user was found with this as a username, raise an error.
+        raise forms.ValidationError('This email address is already in use.')
